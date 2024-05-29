1bf55a46ad9d8134a9ed7024a43ffba504a100df
Ivan Marichev <marichev_iv@pfur.ru>
2024-05-29 12:07:51 +0300

    day 1 task 1

--- day1/m1_t1_hw.go
+++ day1/m1_t1_hw.go
@@ -7,3 +7,29 @@
 
 Выход: стоимость поездки в рублях
 */
+package main
+
+import "fmt"
+
+func main() {
+    var distance, gasConsumption float64
+
+    const gasCost = 48
+
+    fmt.Print("Введите растояние в диапазоне 50 - 10 000 (км): ")
+    fmt.Scanf("%f\n", &distance)
+    if distance < 50 || distance > 10000 {
+        fmt.Println("Вы вышли за пределы допустимого диапазона!")
+        return
+    }
+
+    fmt.Print("Введите расход на 100км диапазоне 5 - 25 (литров): ")
+    fmt.Scanf("%f\n", &gasConsumption)
+    if gasConsumption < 5 || gasConsumption > 25 {
+        fmt.Println("Вы вышли за пределы допустимого диапазона!")
+        return
+    }
+
+    fmt.Printf("Стоимость вашей поездки: %.2f\n", (distance / 100) * gasConsumption * gasCost )
+
+}
