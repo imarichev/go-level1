408d381ec9bb9d9b8a7766a66ecd1c2355f89b44
Ivan Marichev <marichev_iv@pfur.ru>
2024-05-29 12:59:38 +0300

    day 1 task 4

--- day1/m2_t2_hw.go
+++ day1/m2_t2_hw.go
@@ -4,3 +4,29 @@
 Вход: 1221  Выход: 1221 - палиндром
 Вход: 1234  Выход: 1234 - не палиндром
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var number int
+	fmt.Print("Введите четырехзначное число: ")
+	fmt.Scanf("%d\n", &number)
+
+	if number % 10000 != number || number / 1000 == 0 {
+		fmt.Println("Вы ввели не четырехзначное число!")
+		return
+	} 
+
+	first := number / 1000
+	second := (number % 1000) / 100
+	third := (number % 100) / 10
+	fourth := (number % 10)
+
+	if first == fourth && second == third {
+		fmt.Printf("%d - палиндром\n", number)
+	} else {
+		fmt.Printf("%d - не палиндром\n", number)
+	}
+}
