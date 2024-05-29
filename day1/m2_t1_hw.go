20279fbb815ecfdfe6dde8fcd06bb8e266332f0d
Ivan Marichev <marichev_iv@pfur.ru>
2024-05-29 12:42:14 +0300

    day 1 task 3

--- day1/m2_t1_hw.go
+++ day1/m2_t1_hw.go
@@ -4,3 +4,39 @@
 Вход: 1, 9, 2
 Выход: 1, 2, 9
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var first, second, third int
+
+	fmt.Print("Введите три числа через пробел: ")
+	fmt.Scanf("%d %d %d\n", &first, &second, &third)
+
+	fmt.Print("Числа в порядке возрастания: ")
+	if first < second && first < third{
+		fmt.Printf("%d ", first)
+		if second < third {
+			fmt.Printf("%d %d\n", second, third)
+		}else{
+			fmt.Printf("%d %d\n", third, second)
+		}
+	}else if second < third {
+		fmt.Printf("%d ", second)
+		if first < third {
+			fmt.Printf("%d %d\n", first, third)
+		} else {
+			fmt.Printf("%d %d\n", third, first)
+		}
+	} else {
+		fmt.Printf("%d ", third)
+		if first < second {
+			fmt.Printf("%d %d\n", first, second)
+		} else {
+			fmt.Printf("%d %d\n", second, first)
+		}
+	}
+		
+}
