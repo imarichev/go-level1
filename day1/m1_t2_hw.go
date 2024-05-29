2799983f287dcfbf51a9aca780f55a65304e9ccd
Ivan Marichev <marichev_iv@pfur.ru>
2024-05-29 12:29:23 +0300

    day 1 task 2

--- day1/m1_t2_hw.go
+++ day1/m1_t2_hw.go
@@ -4,3 +4,24 @@
 вход: 346, выход: 643
 вход: 100, выход: 001
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var number int
+	fmt.Print("Введите трехзначное число: ")
+	fmt.Scanf("%d\n", &number)
+
+	if number % 1000 != number || number / 100 == 0 {
+		fmt.Println("Вы ввели не трехзначное число!")
+		return
+	}
+
+	first := number / 100
+	second := (number % 100) / 10
+	third := number % 10
+
+	fmt.Printf("Реверсивная запись: %d%d%d\n", third, second, first)
+}
