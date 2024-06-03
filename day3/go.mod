70e88efc53103fee26eb1e813537856417dfa504
Ivan Marichev <marichev_iv@pfur.ru>
2024-06-03 20:46:55 +0300

    day 3 task 1

---
+++ day3/go.mod
@@ -0,0 +1,7 @@
+module example.com/day3
+
+go 1.22.3
+
+replace example.com/readers => ../lib/readers
+
+require example.com/readers v0.0.0-00010101000000-000000000000
--- day3/m5_t2_hw.go
+++ day3/m5_t2_hw.go
@@ -1,11 +1,56 @@
-/* 
+/*
 Задача №1
-Написать функцию, которая расшифрует строку. 
+Написать функцию, которая расшифрует строку.
 code = "220411112603141304"
 Каждые две цифры - это либо буква латинского алфавита в нижнем регистре либо пробел.
 Отчет с 00 -> 'a' и до 25 -> 'z', 26 -> ' '(пробел).
 Вход: строка из цифр. Выход: Текст.
 Проверка работы функции выполняется через вторую строку.
 
-codeToString(code) -> "???????' 
+codeToString(code) -> "???????'
 */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+
+	"example.com/readers"
+)
+
+func main() {
+	fmt.Println("Введите закодированную строку:")
+	code := readers.ReadString()
+	answer, err := decode(code)
+	if err != nil {
+		log.Fatal("Вы ввели некорректную строку!", err)
+	}
+	fmt.Println(answer)
+}
+
+func decode(code string) (string, error) {
+
+	var symbol, answer string
+	key := "abcdefghijklmnopqrstuvwxyz "
+
+	for _, letter := range code {
+		symbol += string(letter)
+		if len(symbol) < 2 {
+			continue
+		}
+		number, err := strconv.ParseInt(symbol, 10, 0)
+		if err != nil {
+			return "", err
+		}
+		if number < 0 || number > 26 {
+			return "", errors.New(fmt.Sprintf("Index out of range: %d", number))
+		}
+		answer += string(key[number])
+		symbol = ""
+	}
+
+	return answer, nil
+}
